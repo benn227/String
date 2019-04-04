66bb8d7971bde50bf21bae11776613180633449b
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-04 05:28:23 +0000

    had enough

--- string.c
+++ string.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+void rotationFunction(char *array, int n);
+
+int main() {
+    int n;
+    char array[]="abc";
+    n=sizeof(array)/sizeof(array[0]);
+    rotationFunction(array, n);
+    return 0;
+}
+
+void rotationFunction(char *array, int n) {
+    int i;
+    for (i=0;i<n;i++) {
+        printf("%c", array[i]);
+    }
+}
