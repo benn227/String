827044c7cd61d1efba9e4ccc78db81986b001512
Ben Newman <benjamin.newman@uon.edu.au>
2019-04-09 21:18:11 +0000

    working without scanf

--- string.c
+++ string.c
@@ -3,16 +3,16 @@
 void rotationFunction(char *array);
 
 int main() {
-    char array[20];
+    char array[20]="test";
     printf("Enter message\n");
-    scanf("%c", &array[20]);
+    //scanf("%c", &array[20]);
     rotationFunction(array);
     return 0;
 }
 
 void rotationFunction(char *array) {
     int i;
-    for (i=0;i!=0;i++) {
+    for (i=0;i<5;i++) {
         printf("%c", array[i]);
     }
     
